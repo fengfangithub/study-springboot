bc4927b75cf56d773f723b6ebe1127718ee540f5
fengfan <15202849186@163.com>
2022-05-10 11:27:09 +0800

    springboot整合web开发笔记初始化配置

---
+++ web/src/main/java/com/fengfan/web/dao/TableDao.java
@@ -0,0 +1,14 @@
+package com.fengfan.web.dao;
+
+import com.fengfan.basis.entity.Table;
+
+import java.util.List;
+
+/**
+ * @author fengfan
+ * @description TODO
+ * @date 2022/5/10 9:39
+ */
+public interface TableDao {
+    List<Table> queryAll();
+}
---
+++ web/src/main/java/com/fengfan/web/entity/Table.java
@@ -0,0 +1,14 @@
+package com.fengfan.web.entity;
+
+import lombok.Data;
+
+/**
+ * @author fengfan
+ * @description 表实体
+ * @date 2022/5/10 9:39
+ */
+@Data
+public class Table {
+    private Integer id;
+    private String name;
+}
